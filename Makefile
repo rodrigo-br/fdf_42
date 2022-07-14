7090f3f8b945946b37422b516001bc81efdab851
rodrigo-br <rodrigoab123@gmail.com>
2022-07-14 23:22:06 +0200

    preparando pastase makefile

---
+++ Makefile
@@ -0,0 +1,25 @@
+NAME	=	fdf
+
+FLAGS	=	-Wall -Wextra -Werror
+
+RM		=	rm -f
+
+.c.o:
+			cc ${FLAGS} -c $< -o ${<:.c=.o} -I.
+
+$(NAME):	${OBJS} ${H_SRC}
+			make -C ./printf
+			mv ./printf/libftprintf.a ./$(NAME)
+
+all: 		${NAME}
+
+clean:
+			${RM} ${OBJS}
+			make clean -C ./printf
+
+fclean:		clean
+			${RM} ${NAME}
+
+re:			fclean all
+
+.PHONY:		all clean fclean re
---
+++ ft_fdf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fdf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralves-b <ralves-b@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 22:57:39 by ralves-b          #+#    #+#             */
+/*   Updated: 2022/07/14 23:18:53 by ralves-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FDF_H
+# define FT_FDF_H
+
+# include "printf/ft_printf.h"
+# include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
